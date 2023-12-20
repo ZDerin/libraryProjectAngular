5efddd799c0605bbf8e6ec8edddfb9c43c102a28
LillySeiffert <lilly.seiffert@brightstraining.com>
2023-12-20 13:30:02 +0100

    implementiere startseite, header, footer

---
+++ src/app/startseite/startseite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startseite',
+  standalone: true,
+  imports: [],
+  templateUrl: './startseite.component.html',
+  styleUrl: './startseite.component.css'
+})
+export class StartseiteComponent {
+
+}
