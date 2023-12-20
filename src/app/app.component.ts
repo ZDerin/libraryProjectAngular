a3eef72579f6714e6a7f3c5a9569a8e4c1e7ecbe
LillySeiffert <lilly.seiffert@brightstraining.com>
2023-12-20 13:23:39 +0100

    implementiere startseite, header, footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
+import {StartseiteComponent} from "./startseite/startseite.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, StartseiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
