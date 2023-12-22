ace63024c89828d97f67479340c4eb43fc20dafc
LillySeiffert <lilly.seiffert@brightstraining.com>
2023-12-22 15:43:37 +0100

    implementiere startseite, header, footer

--- src/app/hauptseite/hauptseite.component.ts
+++ src/app/hauptseite/hauptseite.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit  } from '@angular/core';
 import {BuecherService} from "../buecher.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-hauptseite',
   standalone: true,
   imports: [
-  NgForOf
-],
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './hauptseite.component.html',
   styleUrl: './hauptseite.component.css'
 })
 export class HauptseiteComponent {
   books: any[] = [];
-
+  batchesOfBooks: any[] = [];
   constructor(private buecherService: BuecherService) {}
 
   ngOnInit() {
     // Rufe den Service auf, um Mock-Daten zu erhalten
     this.books = this.buecherService.getBooks();
+    while(this.books.length) {
+      this.batchesOfBooks.push(this.books.splice(0,3))
+    }
   }
 }
