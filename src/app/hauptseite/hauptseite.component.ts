9b2630d0da4ca184b640301d24b774774affc49c
LeonieFranziskavande <leoniefranziska.vanderhorst@brightstraining.com>
2024-01-04 16:58:01 +0100

    versuch, ladeanimation laufen zu lassen (funktioniert noch nicht)

--- src/app/hauptseite/hauptseite.component.ts
+++ src/app/hauptseite/hauptseite.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit  } from '@angular/core';
 import {BuecherService} from "../buecher.service";
 import {CommonModule, NgForOf, NgIf} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
+import {LadevorgangService} from "../ladevorgang.service";
 
 @Component({
   selector: 'app-hauptseite',
@@ -18,13 +19,15 @@ import {HttpClientModule} from "@angular/common/http";
 export class HauptseiteComponent implements OnInit{
   readingWishlist: any[] = [];
   batchesOfBooks: any[] = [];
-  constructor(private buecherService: BuecherService) {}
+  constructor(private buecherService: BuecherService,
+              private ladevorgangService: LadevorgangService) {}
 
   currentUser: string|null = localStorage.getItem('username');
 
   ngOnInit() {
     //this.readingWishlist = this.buecherService.getStandortListe();
     // Rufe den Service auf, um Mock-Daten zu erhalten
+    this.ladevorgangService.showLoader();
     this.buecherService.getStandortListe().subscribe({
       next: value => {
 
@@ -34,6 +37,7 @@ export class HauptseiteComponent implements OnInit{
         while(this.readingWishlist.length) {
           this.batchesOfBooks.push(this.readingWishlist.splice(0,3))
         }
+        this.ladevorgangService.hideLoader(); //??????? hier wurde versucht, eine Ladeanimation einzufügen
       },
       error: err => {
         window.alert("Wunschlist konnte nicht erreicht werden!")
